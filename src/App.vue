2908db28dd22fdb3392c7d6447b0eeaf2555b518
Chars <suncaichao@test.com>
2023-07-08 11:24:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ElConfigProvider } from "element-plus";
+</script>
+
+<template>
+  <el-config-provider size="large">
+    <el-form inline>
+      <el-form-item label="诉求内容">
+        <el-input />
+      </el-form-item>
+      <el-form-item label="诉求地址">
+        <el-input />
+      </el-form-item>
+      <el-form-item label="诉求来源">
+        <el-select />
+      </el-form-item>
+    </el-form>
+  </el-config-provider>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/styles/element.scss" as el;
+        `
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+          // directives: true,
+          // version: "2.1.5",
+        }),
+      ],
+    }),
+  ],
+})
